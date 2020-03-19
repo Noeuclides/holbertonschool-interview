287bd4c222f8b9e84615500d2180885d9cb821a4
Noeuclides <euclidesnoeuclides@gmail.com>
2020-03-19 04:10:43 +0000

    log parsing python

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
+import sys
 
+sizes = []
+try:
+    for line in sys.stdin:
+        array = line.split(' - ')
+        ip = array[0]
+        get = array[1].split(' ')
+        code = get[-2]
+        size = get[-1]
+        if len(sizes) == 10:
+            print("File size: {}".format(sum(sizes)))
+            sorted(sizes)
+            for size in sizes:
+                print("status code: {}".format(size))
+                sizes = []
+        else:
+            sizes.append(int(size))
+except Exception:
+    pass
+finally:
+    print("File size: {:d}".format(sum(sizes)))
+    for size in sizes:
+        print("status code: {}".format(size))
