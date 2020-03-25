19bcf99085489907e03bb73fa3a374b359d5650f
Noeuclides <euclidesnoeuclides@gmail.com>
2020-03-25 14:24:31 +0000

    check if a linked list has a cycle

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ *
+ * @list: pointer to the head of the linked list
+ *
+ * Return: 1 if there is a cycle, 0 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+
+	turtle = list;
+	hare = list;
+
+	while (hare->next->next != NULL)
+	{
+		hare = hare->next->next;
+		turtle = turtle->next;
+
+		if (hare == turtle)
+			return (1);
+	}
+
+	return (0);
+}
