f20dd1ba9b661226968ee8458a3582f781c48ee5
Noeuclides <euclidesnoeuclides@gmail.com>
2020-04-04 11:16:51 +0000

    log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 import sys
 
-sizes = []
+count = 0
+total_size = 0
+status_code = ['200', '301', '400', '401', '400', '404', '405', '500']
+code_count = {}
+
 try:
     for line in sys.stdin:
-        array = line.split(' - ')
-        ip = array[0]
-        get = array[1].split(' ')
-        code = get[-2]
-        size = get[-1]
-        if len(sizes) == 10:
-            print("File size: {}".format(sum(sizes)))
-            sorted(sizes)
-            for size in sizes:
-                print("status code: {}".format(size))
-                sizes = []
-        else:
-            sizes.append(int(size))
+        array = line.split(' ')
+        if len(array) > 2:
+            code = array[-2]
+            size = array[-1]
+            count += 1
+            total_size += int(size)
+            if code in status_code:
+                if code in code_count:
+                    code_count[code] += 1
+                else:
+                    code_count[code] = 1
+        if count == 10:
+            print("File size: {}".format(total_size))
+            status = sorted(code_count.items())
+            for k, v in status:
+                print("{}: {}".format(k, v))
+            count = 0
 except Exception:
     pass
 finally:
-    print("File size: {:d}".format(sum(sizes)))
-    for size in sizes:
-        print("status code: {}".format(size))
+    print("File size: {:d}".format(total_size))
+    status = sorted(code_count.items())
+    for k, v in status:
+        print("{}: {}".format(k, v))
