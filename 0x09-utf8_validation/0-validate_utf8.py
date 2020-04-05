49449b7e5a0a10c6282beeefb0cc89bd7d9e84ab
Noeuclides <euclidesnoeuclides@gmail.com>
2020-04-05 02:39:57 +0000

    UFT8 validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    count = 0
+    for byte in data:
+        binary = bin(byte).replace('0b', '').rjust(8, '0')
+        if count == 0:
+            if binary.startswith('110'):
+                count = 1
+            if binary.startswith('1110'):
+                count = 2
+            if binary.startswith('11110'):
+                count = 3
+            if binary.startswith('10'):
+                return False
+        else:
+            if not binary.startswith('10'):
+                return False
+            count -= 1
+
+    return True
