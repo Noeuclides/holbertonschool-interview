b22815a0419ca3f94f2e70ffab910f1de3f002f0
Noeuclides <euclidesnoeuclides@gmail.com>
2020-04-17 09:43:06 +0000

    unlock boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,7 +8,7 @@ def canUnlockAll(boxes):
     """determine if all the boxes can be opened
     """
     keys = sorted(list(set(boxes[0])))
-    keys_used = []
+    keys_used = [0]
     count = 1
 
     if not keys:
@@ -16,15 +16,19 @@ def canUnlockAll(boxes):
 
     index = keys[0]
 
-    a.remove(8)
-    while index < len(boxes) or keys:
-        keys.append(boxes[index])
-        keys = sorted(list(set(boxes[index])))
+    while index < len(boxes) - 1 and keys:
+        keys.extend(boxes[index])
+        keys = sorted(list(set(keys)))
         keys.remove(index)
-        index = keys[0]
+        keys_used.append(index)
+        for k in keys_used:
+            if k in keys:
+                keys.remove(k)
+        if keys:
+            index = keys[0]
         count += 1
 
-    if count == len(boxes):
+    if count == len(boxes) - 1:
         return True
 
     return False
