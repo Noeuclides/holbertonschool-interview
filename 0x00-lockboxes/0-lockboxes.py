4b0184b829d1da4c345c12dc9884ec83414ec3f7
No Euclides <euclidesnoeuclides@gmail.com>
2020-04-17 04:08:47 -0500

    unlock boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,24 +7,25 @@ Module
 def canUnlockAll(boxes):
     """determine if all the boxes can be opened
     """
-    if boxes[0] is None:
+    keys = sorted(list(set(boxes[0])))
+    keys_used = []
+    count = 1
+
+    if not keys:
         return False
 
-    len_boxes = [i for i in range(len(boxes))]
-    state = [0 for i in range(len(boxes))]
-    box_keys = dict(zip(len_boxes, state))
-    box_keys[0] = 1
-    keys = [key for key in boxes[0]]
-    k = 0
-    while k in range(len(keys)):
-        if keys[k] < len(boxes) and boxes[keys[k]]:
-            for ki in boxes[keys[k]]:
-                keys.append(ki)
-            box_keys[keys[k]] = 1
+    index = keys[0]
 
-        k = keys[k]
-    box_keys[k] = 1
-    for v in box_keys.values():
-        if v == 0:
-            return False
-    return True
+    a.remove(8)
+    while index < len(boxes) or keys:
+        keys.append(boxes[index])
+        keys = sorted(list(set(boxes[index])))
+        keys.remove(index)
+        index = keys[0]
+        count += 1
+
+    if count == len(boxes):
+        return True
+
+    return False
+    
