4898c8c1de1359a752cf95b727c6c97ff328cfe7
Noeuclides <euclidesnoeuclides@gmail.com>
2020-07-01 20:15:00 +0000

    include all functions in the header file

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,14 +1,15 @@
 #ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
 #define SLIDE_LEFT 0
 #define SLIDE_RIGHT 1
 
 
-int slide_line(int *line, size_t size, int direction);
+#include <stdio.h>
+#include <stdlib.h>
 
+int slide_line(int *line, size_t size, int direction);
+void slide_left(int *line, size_t size);
+void slide(int *line, size_t size);
+void reverse_line(int *line, size_t size);
 
 #endif /* SLIDE_LINE_H */
