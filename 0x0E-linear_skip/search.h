16af7b50188a8199f0d2d56a381cb4b82a789c60
Noeuclides <euclidesnoeuclides@gmail.com>
2020-11-11 18:53:56 +0000

    -_-

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -1,11 +1,10 @@
-#ifndef GRANDPARENT_H
-#define GRANDPARENT_H
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
 
-
-
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
@@ -24,13 +23,12 @@ typedef struct skiplist_s
 	size_t index;
 	struct skiplist_s *next;
 	struct skiplist_s *express;
-
 } skiplist_t;
 
-skiplist_t *linear_skip(skiplist_t *list, int value);
 skiplist_t *create_skiplist(int *array, size_t size);
 void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
-skiplist_t *check_next(skiplist_t *node, int value);
+skiplist_t *linear_skip(skiplist_t *head, int value);
+skiplist_t *linear_search(skiplist_t *min, skiplist_t *max, int value);
 
-#endif /* GRANDPARENT_H */
+#endif /* _SEARCH_H_ */
