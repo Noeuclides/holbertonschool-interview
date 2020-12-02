62337cc43e9129e9696c0ef3843ab155b05a756f
Noeuclides <euclidesnoeuclides@gmail.com>
2020-12-02 22:09:21 +0000

    heap sort

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef _HEAP_SORT_H_
+#define _HEAP_SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+
+
+#endif /* _HEAP_SORT_H_ */
